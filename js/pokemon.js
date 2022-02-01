150552ea50f620ca9f140b8d9adf84df44f9e959
Benjamin Banwart <benjibanwart@gmail.com>
2022-01-31 21:31:48 -0600

    added 'type' to pokemon and moves, and added third pokemon: pikachu

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,9 +1,10 @@
 //pokemon class works as blueprint for each individual pokemon
 class Pokemon {
-    constructor (name, health, image, m1, m2, m3, m4){
+    constructor (name, health, image, type, m1, m2, m3, m4){
         this.name = name;
         this.health = health;
         this.image = image;
+        this.type = type;
         this.m1 = m1;
         this.m2 = m2;
         this.m3 = m3;
@@ -12,13 +13,14 @@ class Pokemon {
 }
 
 //pokemon are created here
-var charizard = new Pokemon("Charizard", 150, "./assets/pngfind.com-charizard-sprite-png-4458716-min.png", () => {return {name: "cut", damage: 30}}, () => {return {name: "flamethrower", damage: 45}}, () => {return {name: "dragonRage", damage: 70}}, () => {return {name: "rockSlide", damage: 60}});
-var gyarados = new Pokemon("Gyarados", 140, "./assets/oie_124642GgjIgslR.png", () => {return {name: "tackle", damage: 30}}, () => {return {name: "hydroPump", damage: 50}}, () => {return {name: "dragonRage", damage: 70}}, () => {return {name: "surf", damage: 45}})
+var charizard = new Pokemon("Charizard", 150, "./assets/pngfind.com-charizard-sprite-png-4458716-min.png", ['fire', 'dragon'], () => {return {name: "cut", damage: 30, type: "normal"}}, () => {return {name: "flamethrower", damage: 45, type: "fire"}}, () => {return {name: "dragonRage", damage: 70, type: "dragon"}}, () => {return {name: "rockSlide", damage: 60, type: "rock"}});
+var gyarados = new Pokemon("Gyarados", 140, "./assets/oie_124642GgjIgslR.png", ['water', 'dragon'], () => {return {name: "tackle", damage: 30, type: "normal"}}, () => {return {name: "hydroPump", damage: 50, type: "water"}}, () => {return {name: "dragonRage", damage: 70, type: "dragon"}}, () => {return {name: "surf", damage: 45, type: "water"}})
+var pikachu = new Pokemon("Pikachu",100, "./assets/pngegg-min.png", ['electric'], () => {return {name: "tackle", damage: 30, type: "normal"}}, () => {return {name: "thunderPunch", damage: 40, type: "electric"}}, () => {return {name: "Thunderbolt", damage: 60, type: "electric"}}, () => {return {name: "mudSlap", damage: 50, type: "ground"}})
 
 //user's pokemon is placed onto the battlefield
 function createUserPokemon() {
     const img = document.createElement('img');
-    img.src = gyarados.image;
+    img.src = charizard.image;
     img.className = 'img-size';
     document.getElementById('playerPokemon').appendChild(img);
 }
