a8a94ab3779cae700e151525e57234321a19fe0a
Benjamin Banwart <benjaminbanwart@Benjamins-MacBook-Pro.local>
2022-02-02 11:22:38 -0600

    added new pokemon: bulbasaur

--- js/pokemon.js
+++ js/pokemon.js
@@ -15,7 +15,8 @@ class Pokemon {
 //pokemon are created here
 var charizard = new Pokemon("Charizard", 150, "./assets/pngfind.com-charizard-sprite-png-4458716-min.png", ['fire', 'dragon'], () => {return {name: "cut", damage: 30, type: "normal"}}, () => {return {name: "flamethrower", damage: 45, type: "fire"}}, () => {return {name: "dragonRage", damage: 70, type: "dragon"}}, () => {return {name: "rockSlide", damage: 60, type: "rock"}});
 var gyarados = new Pokemon("Gyarados", 140, "./assets/oie_124642GgjIgslR.png", ['water', 'dragon'], () => {return {name: "tackle", damage: 30, type: "normal"}}, () => {return {name: "hydroPump", damage: 50, type: "water"}}, () => {return {name: "dragonRage", damage: 70, type: "dragon"}}, () => {return {name: "surf", damage: 45, type: "water"}})
-var pikachu = new Pokemon("Pikachu",100, "./assets/pngegg-min.png", ['electric'], () => {return {name: "tackle", damage: 30, type: "normal"}}, () => {return {name: "thunderPunch", damage: 40, type: "electric"}}, () => {return {name: "Thunderbolt", damage: 60, type: "electric"}}, () => {return {name: "mudSlap", damage: 50, type: "ground"}})
+var pikachu = new Pokemon("Pikachu",100, "./assets/pngegg-min.png", ['electric'], () => {return {name: "tackle", damage: 30, type: "normal"}}, () => {return {name: "thunderPunch", damage: 40, type: "electric"}}, () => {return {name: "thunderbolt", damage: 60, type: "electric"}}, () => {return {name: "mudSlap", damage: 50, type: "ground"}})
+var bulbasaur = new Pokemon("Bulbasaur", 90, "./assets/oie_218129sCzeiPaG-min.png", ['grass'], () => {return {name: "cut", damage: 30, type: "normal"}}, () => {return {name: "vineWhip", damage: 35, type: "grass"}}, () => {return {name: "razorLeaf", damage: 45, type: "grass"}}, () => {return {name: "mudSlap", damage: 50, type: "ground"}})
 
 //user's pokemon is placed onto the battlefield
 function createUserPokemon() {
@@ -28,7 +29,7 @@ function createUserPokemon() {
 //computer's pokemon is placed onto the battlefield
 function createComputerPokemon() {
     const img = document.createElement('img');
-    img.src = charizard.image;
+    img.src = gyarados.image;
     img.className = 'img-size';
     document.getElementById('computerPokemon').appendChild(img);
 }
