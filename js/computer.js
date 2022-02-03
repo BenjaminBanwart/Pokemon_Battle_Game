e426bdc8da5a61ffdd4f2541eeab57628780700b
Benjamin Banwart <benjibanwart@gmail.com>
2022-02-02 20:04:14 -0600

    moved createPokemon functions into classes as methods

--- js/computer.js
+++ js/computer.js
@@ -1,15 +1,15 @@
 const cpu = {
     turn: false,
     pokemon: [],
-    currentPokemon: []
+    currentPokemon: [],
+
+    //computer's pokemon is placed onto the battlefield
+    createComputerPokemon() {
+        const img = document.createElement('img');
+        img.src = gengar.image;
+        img.className = 'img-size';
+        document.getElementById('computerPokemon').appendChild(img);
+    }
 }
 
-//computer's pokemon is placed onto the battlefield
-function createComputerPokemon() {
-    const img = document.createElement('img');
-    img.src = gengar.image;
-    img.className = 'img-size';
-    document.getElementById('computerPokemon').appendChild(img);
-}
-
-createComputerPokemon()
+cpu.createComputerPokemon()
--- js/user.js
+++ js/user.js
@@ -1,15 +1,15 @@
 const user = {
     turn: true,
     pokemon: [],
-    currentPokemon: []
+    currentPokemon: [],
+
+    //user's pokemon is placed onto the battlefield
+    createUserPokemon() {
+        const img = document.createElement('img');
+        img.src = snivy.image;
+        img.className = 'img-size';
+        document.getElementById('playerPokemon').appendChild(img);
+    }
 }
 
-//user's pokemon is placed onto the battlefield
-function createUserPokemon() {
-    const img = document.createElement('img');
-    img.src = snivy.image;
-    img.className = 'img-size';
-    document.getElementById('playerPokemon').appendChild(img);
-}
-
-createUserPokemon()
+user.createUserPokemon()
