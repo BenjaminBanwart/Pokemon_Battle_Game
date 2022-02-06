b1ad541b2bef65ca4ded5eb13709faeae31ee305
Benjamin Banwart <benjibanwart@gmail.com>
2022-02-06 14:57:26 -0600

    repositioned attack move buttons

--- js/fight.js
+++ js/fight.js
@@ -1,6 +1,6 @@
 async function userFightButtons() {
-    await getCurrentPokemon()
-    let button1 = document.getElementById('attack1').innerHTML = user.currentPokemon[0].m1.name;
+    await getCurrentPokemon();
+    let button1 = await document.getElementById('attack1').innerHTML = user.currentPokemon[0].m1.name;
 }
 userFightButtons()
 console.log(user.currentPokemon[0].m1.name)
